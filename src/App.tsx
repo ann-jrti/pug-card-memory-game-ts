69432743bc9992daae5e5b28d7e31e3d18ac203e
ann <asog.jrti@gmail.com>
2022-04-11 21:57:16 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { createBoard } from './setup';
+import { Grid } from './App.styles';
 
-function App() {
+const App = () => {
+  console.log(createBoard());
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Grid>Start Here!</Grid>
     </div>
   );
-}
+};
 
 export default App;
